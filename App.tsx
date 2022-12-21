0d0f1c4cb258f6f88f0b8956bbfa662b0e3a0813
Vitor Emidio <vitorsemidio.96@gmail.com>
2022-12-21 18:49:34 -0300

    chore(config): setup path mapping

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import { TodoApp } from '@screens/TodoApp';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
@@ -6,6 +7,7 @@ export default function App() {
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <TodoApp />
     </View>
   );
 }
---
+++ src/screens/TodoApp/index.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@components/Button';
+import { Text, View } from 'react-native';
+
+export function TodoApp() {
+  return (
+    <View>
+      <Text>TodoApp</Text>
+      <Button />
+    </View>
+  );
+}
